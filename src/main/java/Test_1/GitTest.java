b913e298be2b15471fa4cb618140e69e6fa9e0ac
yileina <2742387136@qq.com>
2022-09-18 01:06:31 +0800

    first commit

---
+++ src/main/java/Test_1/GitTest.java
@@ -0,0 +1,12 @@
+package Test_1;
+
+/**
+ * @author 伊雷娜
+ * @version 1.0
+ * @date 2022/9/18 1:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
